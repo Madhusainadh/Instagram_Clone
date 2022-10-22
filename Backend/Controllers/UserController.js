1519aab9630d9b99bf0778ac79e71171b1cae4b5
madhusainadh <lalammadhusainadh150@gmail.com>
2022-10-22 23:46:05 +0530

    working on backend

--- Backend/Controllers/UserController.js
+++ Backend/Controllers/UserController.js
@@ -1,9 +1,10 @@
 import userModel from "../Models/UserModel.js";
-
+import bcrypt from "bcrypt"
 // get a User
 
 export const getUser = async (req,res)=>{
     const id = req.params.id;
+   
     try{
         const user = await userModel.findById(id)
 
@@ -23,9 +24,49 @@ export const getUser = async (req,res)=>{
 export const updateUser = async(req,res)=>{
 const id = req.params.id;
 
-const {currentUser,currentUserAdminStatus,password} = req.body;
+console.log(id)
+const {currentUserId,currentUserAdminStatus,password} = req.body;
 
+
+if(id===currentUserId||currentUserAdminStatus){
+    try{
+
+        if(password){
+            const salt =await bcrypt.genSalt(10);
+            req.body.password = await bcrypt.hash(password,salt)
+        }
+        console.log("try")
+         const user = await userModel.findByIdAndUpdate(id,req.body,{new:true})
+         console.log(user)
+        res.status(200).json(user)
+    }catch(err){
+        console.log(err.message)
+        res.status(500).json({message:err.message})
+    }
+}else{
+    res.status(403).json("Access Denied! you can only update your profile")
+}
  
+}
 
+export const deleteUser=async(req,res)=>{
+    const id = req.params.id
+    const {currentUserId,currentUserAdminStatus}= req.body;
+    if(currentUserId==id||currentUserAdminStatus){
+        try{
+await userModel.findByIdAndDelete(id)
+res.status(200).json("User deleted successfully")
+        }catch(err){
+        console.log(err.message)
+        res.status(500).json({message:err.message})
+    }
+    }else{
+        res.status(403).json("Access Denied! you can only delete your profile")
+    }
+}
 
+export const followUser = async(req,res)=>{
+    const id = req.params.id;
+    const {currentUserId} = req.body;
+    
 }
